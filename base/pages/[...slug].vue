cdd9c66616c90254bc689613d798c857d370f857
Tadai <tadai@jota.one>
2023-11-19 22:34:22 +0100

    chore: lint main page cmpt

--- base/pages/[...slug].vue
+++ base/pages/[...slug].vue
@@ -1,9 +1,5 @@
 <template>
-  <main>
-    <ContentDoc>
-      <template #not-found>
-        NOT FOUND
-      </template>
-    </ContentDoc>
-  </main>
+  <ContentDoc>
+    <template #not-found> NOT FOUND </template>
+  </ContentDoc>
 </template>
