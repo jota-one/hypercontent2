77b3fcd9527c6a9dda3b1170c39a9b41728b3e75
Tadai <tadai@jota.one>
2023-11-19 22:30:58 +0100

    chore: cleanup app.vue

--- base/app.vue
+++ base/app.vue
@@ -3,11 +3,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<script setup lang="ts">
-// import useLangs from './composables/useLangs'
-
-// const { loadLangs } = useLangs()
-// await loadLangs()
-// console.log(currentLang.value)
-</script>
