fd546c5bfa45c32aa98c487c0969e34ccdb6807a
Tadai <tadai@jota.one>
2023-11-26 18:57:14 +0100

    feat: add default layout ot playground

---
+++ base/.playground/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      Header ({{ currentLangCode }})
+    </div>
+  </header>
+  <main class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+    <slot/>
+  </main>
+  <footer>
+    <div class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      Footer
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const { currentLangCode } = useHcLangs()
+</script>
