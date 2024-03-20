0b77214088100835363f6bb790e1a51beb43286a
Tadai <tadai@jota.one>
2024-03-20 16:30:04 +0100

    feat: use HcWebsite and hcPage in playground demo app

--- cms/.playground/layouts/default.vue
+++ cms/.playground/layouts/default.vue
@@ -1,17 +1,28 @@
 <template>
-  <header>
-    <div class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-      Header ({{ currentLangCode }})
-    </div>
-  </header>
-  <main class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-    <slot/>
-  </main>
-  <footer>
-    <div class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-      Footer
-    </div>
-  </footer>
+  <HcWebsite>
+    <header>
+      <div
+        class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+      >
+        Header ({{ currentLangCode }})
+      </div>
+    </header>
+    <main
+      class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+    >
+      <HcPage>
+        <slot />
+        <template #restricted-access> Restricted </template>
+      </HcPage>
+    </main>
+    <footer>
+      <div
+        class="mx-auto flex max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+      >
+        Footer
+      </div>
+    </footer>
+  </HcWebsite>
 </template>
 
 <script setup lang="ts">
